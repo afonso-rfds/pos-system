ace46673e04cbe6a4ae63916f8be50d783f11a38
afonso-rfds <afonso.rfds@gmail.com>
2024-09-13 18:39:31 +0100

    [master] Description for Product class' members

--- pos-supermarket/products/products.hpp
+++ pos-supermarket/products/products.hpp
@@ -8,8 +8,15 @@ class Product
     Product()  = default;
     ~Product() = default;
 
+    /// @brief EAN13 that uniquely identifies the product
     std::string EAN13;
+
+    /// @brief Name of the product
     std::string productName;
+
+    /// @brief Price of the product
     double price;
+
+    /// @brief Tax rate associated with the product
     double taxRate;
 };
