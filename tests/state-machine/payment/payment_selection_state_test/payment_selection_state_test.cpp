699eaf6c71e761381ec452e13ecb348480bb4454
afonso-rfds <afonso.rfds@gmail.com>
2024-10-21 22:31:18 +0100

    [master] Tests for PaymentSelectionState

--- tests/state-machine/payment/payment_selection_state_test/payment_selection_state_test.cpp
+++ tests/state-machine/payment/payment_selection_state_test/payment_selection_state_test.cpp
@@ -1,20 +1,55 @@
 
 #include "payment_selection_state_test.hpp"
-#include "state_machine/states/payment/partial_payment_state/partial_payment_state.hpp"
 
-INLINE_FUNCTION void PaymentSelectionStateTest::createCustomInputString(std::string inputString)
+INLINE_FUNCTION void PaymentSelectionTest::createCustomInputString(std::string inputString)
 {
-    std::istringstream input(inputString);
+    std::istringstream input(inputString + "\n");
     std::cin.rdbuf(input.rdbuf());
+    std::cin.clear();
 }
 
-TEST_F(PaymentSelectionStateTest, processState)
+void PaymentSelectionTest::SetUp()
 {
-    // createCustomInputString("mbway");
+    posContext            = new MockContext();
+    paymentSelectionState = new PaymentSelectionState();
+    transactionData       = new TransactionData("Afonso's Supermarket");
+}
 
-    // paymentSelectionState = new PaymentSelectionState();
-    // posContext            = new POSContext(paymentSelectionState, "Afonso's Supermarket");
+void PaymentSelectionTest::TearDown()
+{
+    delete paymentSelectionState;
+    delete posContext;
+    delete transactionData;
+}
 
-    // paymentSelectionState->processState(*posContext);
-    // ASSERT_TRUE(dynamic_cast<PartialPaymentState*>(posContext->getCurrentState()) != nullptr);
+TEST_F(PaymentSelectionTest, enterState)
+{
+    EXPECT_CALL(*posContext, getTransactionData()).Times(3).WillRepeatedly(::testing::ReturnRef(*transactionData));
+
+    paymentSelectionState->enterState(*posContext);
+}
+
+TEST_F(PaymentSelectionTest, processState_invalidInput)
+{
+    createCustomInputString("invalidInput");
+
+    EXPECT_CALL(*posContext, getTransactionData()).WillRepeatedly(::testing::ReturnRef(*transactionData));
+    EXPECT_DEATH({ paymentSelectionState->processState(*posContext); }, "");
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    PaymentSelectionInputs,
+    PaymentSelectionParamTest,
+    ::testing::Values("mbway", "card", "cash"));
+
+TEST_P(PaymentSelectionParamTest, processState_multipleValidInputs)
+{
+    const std::string& inputString = GetParam();
+
+    createCustomInputString(inputString);
+
+    EXPECT_CALL(*posContext, getTransactionData()).Times(1).WillOnce(::testing::ReturnRef(*transactionData));
+    EXPECT_CALL(*posContext, transitionToState(StateType::PartialPayment)).Times(1);
+
+    paymentSelectionState->processState(*posContext);
 }
--- tests/state-machine/payment/payment_selection_state_test/payment_selection_state_test.hpp
+++ tests/state-machine/payment/payment_selection_state_test/payment_selection_state_test.hpp
@@ -1,20 +1,30 @@
 #pragma once
 
-#include "state_machine/context/pos/pos_context.hpp"
-#include "state_machine/states/items_state/items_state.hpp"
+#include "../../../mocks/mock_context.hpp"
 #include "state_machine/states/payment/payment_selection_state/payment_selection_state.hpp"
+#include "state_machine/transaction_data/transaction_data.hpp"
 
 #include <gtest/gtest.h>
 
 #define INLINE_FUNCTION inline __attribute__((always_inline))
 
-class PaymentSelectionStateTest : public ::testing::Test
+class PaymentSelectionTest : public ::testing::Test
 {
   protected:
     /// @brief Create a personalized input string and redirect std::cin
     /// @param inputString string to mock as user's input
     INLINE_FUNCTION void createCustomInputString(std::string inputString);
 
-    POSContext* posContext;
+    void SetUp() override;
+    void TearDown() override;
+
     PaymentSelectionState* paymentSelectionState;
+    TransactionData* transactionData;
+
+    MockContext* posContext;
+};
+
+// Parameterized test case for inputs "mbway", "card", and "cash"
+class PaymentSelectionParamTest : public PaymentSelectionTest, public ::testing::WithParamInterface<std::string>
+{
 };
