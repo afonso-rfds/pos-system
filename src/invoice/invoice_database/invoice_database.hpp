9d7b65eb7a3cc2de81914fc2645daa44f98e5adc
afonso-rfds <afonso.rfds@gmail.com>
2024-10-22 20:14:14 +0100

    [master] Changed pos-supermarket folder's name to 'src'

--- src/invoice/invoice_database/invoice_database.hpp
+++ src/invoice/invoice_database/invoice_database.hpp
@@ -5,7 +5,7 @@
 #include <SQLiteCpp/SQLiteCpp.h>
 #include <string>
 
-const std::string INVOICE_DATABASE_PATH = std::string(PROJECT_ROOT) + "/pos-supermarket/database/invoices.db";
+const std::string INVOICE_DATABASE_PATH = std::string(PROJECT_ROOT) + "/src/database/invoices.db";
 
 class InvoiceDatabase
 {
