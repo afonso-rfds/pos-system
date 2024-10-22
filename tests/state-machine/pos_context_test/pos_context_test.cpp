c563d01a7e1845bc3b98d7e576d59fdffc813d86
afonso-rfds <afonso.rfds@gmail.com>
2024-10-22 14:21:12 +0100

    [master] Commented out incorrect tests (for now)

--- tests/state-machine/pos_context_test/pos_context_test.cpp
+++ tests/state-machine/pos_context_test/pos_context_test.cpp
@@ -1,34 +1,34 @@
 #include "pos_context_test.hpp"
 
-void POSContextTest::SetUp()
-{
-    mockInitialState = new MockPOSState();
-    mockSecondState  = new MockPOSState();
+// void POSContextTest::SetUp()
+// {
+//     mockInitialState = new MockPOSState();
+//     mockSecondState  = new MockPOSState();
 
-    posContext       = new POSContext("Afonso's Supermarket");
-}
+//     posContext       = new POSContext("Afonso's Supermarket");
+// }
 
-void POSContextTest::TearDown()
-{
-    delete posContext;
-}
+// void POSContextTest::TearDown()
+// {
+//     delete posContext;
+// }
 
-TEST_F(POSContextTest, getCurrentState)
-{
-    EXPECT_EQ(posContext->getCurrentState(), mockInitialState);
-}
+// TEST_F(POSContextTest, getCurrentState)
+// {
+//     EXPECT_EQ(posContext->getCurrentState(), mockInitialState);
+// }
 
-TEST_F(POSContextTest, DISABLED_transitionToState)
-{
-    EXPECT_CALL(*mockSecondState, enterState(::testing::Ref(*posContext))).Times(1);
+// TEST_F(POSContextTest, DISABLED_transitionToState)
+// {
+//     EXPECT_CALL(*mockSecondState, enterState(::testing::Ref(*posContext))).Times(1);
 
-    posContext->transitionToState(StateType::Idle);
-    EXPECT_EQ(posContext->getCurrentState(), mockSecondState);
-}
+//     posContext->transitionToState(StateType::Idle);
+//     EXPECT_EQ(posContext->getCurrentState(), mockSecondState);
+// }
 
-TEST_F(POSContextTest, processCurrentState)
-{
-    EXPECT_CALL(*mockInitialState, processState(::testing::Ref(*posContext))).Times(1);
+// TEST_F(POSContextTest, processCurrentState)
+// {
+//     EXPECT_CALL(*mockInitialState, processState(::testing::Ref(*posContext))).Times(1);
 
-    posContext->processCurrentState();
-}
+//     posContext->processCurrentState();
+// }
